5be0c89add51843dc500f87b16c18a4f107cf6c7
jiwlee <53519832+jiwlee97@users.noreply.github.com>
2024-10-25 18:29:24 +0900

    feat: Form 관련 컴포넌트 구현

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,3 +1,12 @@
 export { Button } from './Button';
 export { Input } from './Input';
 export { Label } from './Label';
+export {
+  Form,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+  FormField,
+} from './Form';
