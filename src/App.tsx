1fd5c1cc9589328d66b8b9cb5f03781a56d57f35
jiwlee <53519832+jiwlee97@users.noreply.github.com>
2024-10-27 20:25:31 +0900

    build: tanstack query 및 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import { Button } from "@/shared/ui";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([]);
+
+const queryClient = new QueryClient();
 
 function App() {
-  return <Button>Todo List</Button>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import ErrorPage from "./ErrorPage.tsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-  },
-]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>
 );
