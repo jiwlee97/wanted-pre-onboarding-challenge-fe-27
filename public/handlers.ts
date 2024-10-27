1e2495f80479e789bb34b6f323c815421f52b413
jiwlee <53519832+jiwlee97@users.noreply.github.com>
2024-10-28 00:15:26 +0900

    docs: SignUpPage story 구현

--- public/handlers.ts
+++ public/handlers.ts
@@ -4,8 +4,9 @@ import type { SignUpRequest } from '../src/auth'
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const handlers = [
-  http.post<PathParams, SignUpRequest>(`${BASE_URL}/users/create`, (req) => {
-    console.log(req);
+  http.post<PathParams, SignUpRequest>(`${BASE_URL}/users/create`, async ({ request }) => {
+    const requestBody = await request.json();
+    console.log(requestBody);
     return HttpResponse.json({
       "message": "계정이 성공적으로 생성되었습니다",
       "token": "token"
