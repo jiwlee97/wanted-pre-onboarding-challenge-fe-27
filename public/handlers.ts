d351e2f637ef7aa10518d9e8e4653ba03a326a8e
jiwlee <53519832+jiwlee97@users.noreply.github.com>
2024-10-28 18:32:11 +0900

    chore: login api msw handler 추가

--- public/handlers.ts
+++ public/handlers.ts
@@ -1,10 +1,20 @@
 import { http, HttpResponse, PathParams } from 'msw'
-import type { SignUpRequest } from '../src/auth'
+import type { SignUpRequest } from '../src/pages/sign-up'
+import type { LoginRequest } from '../src/pages/login'
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const handlers = [
   http.post<PathParams, SignUpRequest>(`${BASE_URL}/users/create`, async ({ request }) => {
+    const requestBody = await request.json();
+    console.log(requestBody);
+    return HttpResponse.json({
+      "message": "계정이 성공적으로 생성되었습니다",
+      "token": "token"
+    }, { status: 200 })
+  }),
+
+  http.post<PathParams, LoginRequest>(`${BASE_URL}/users/login`, async ({ request }) => {
     const requestBody = await request.json();
     console.log(requestBody);
     return HttpResponse.json({
--- src/pages/sign-up/index.ts
+++ src/pages/sign-up/index.ts
@@ -1,1 +1,2 @@
 export { SignUpPage } from './ui/SignUpPage';
+export type { SignUpRequest } from './api/useSignUp';
