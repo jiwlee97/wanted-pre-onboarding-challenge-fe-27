fbbf1979c77180e7642f8414202646fcb01cd52f
jiwlee <53519832+jiwlee97@users.noreply.github.com>
2024-10-28 21:17:22 +0900

    feat: DefaultPageLayout css 수정

--- src/shared/ui/layouts/DefaultPageLayout.tsx
+++ src/shared/ui/layouts/DefaultPageLayout.tsx
@@ -12,10 +12,10 @@ const Header = () => {
   };
 
   return (
-    <header className="w-full mr-auto ml-auto pr-8 pl-8 flex flex-col items-start justify-between space-y-2 py-4 sm:flex-row sm:items-center sm:space-y-0 md:h-16">
-      <h2 className="text-lg font-semibold">TodoList</h2>
+    <header className="bg-zinc-900 w-full mr-auto ml-auto pr-8 pl-8 flex flex-col items-start justify-between space-y-2 py-4 sm:flex-row sm:items-center sm:space-y-0 md:h-16">
+      <h2 className="text-white text-lg font-semibold">TodoList</h2>
       <div className="ml-auto flex w-full space-x-2 sm:justify-end">
-        <Button size="sm" onClick={onLogout}>
+        <Button variant="secondary" size="sm" onClick={onLogout}>
           Logout
         </Button>
       </div>
@@ -27,7 +27,7 @@ export const DefaultPageLayout = () => {
   return (
     <div className="overflow-hidden bg-background h-screen w-screen flex flex-col">
       <Header />
-      <main>
+      <main className="flex-1">
         <Outlet />
       </main>
     </div>
