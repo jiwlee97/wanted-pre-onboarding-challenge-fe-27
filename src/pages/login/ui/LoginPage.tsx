823bc9d93bb6cd34f0767a49c4bda364c9acc634
jiwlee <53519832+jiwlee97@users.noreply.github.com>
2024-10-28 18:26:21 +0900

    fix: sign-up 페이지 경로 수정

--- src/pages/login/ui/LoginPage.tsx
+++ src/pages/login/ui/LoginPage.tsx
@@ -5,7 +5,7 @@ export const LoginPage = () => {
   return (
     <>
       <Link
-        to="/signup"
+        to="/sign-up"
         className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 hover:bg-accent hover:text-accent-foreground h-9 px-4 py-2 absolute right-4 top-4 md:right-8 md:top-8 text-black"
       >
         SignUp
