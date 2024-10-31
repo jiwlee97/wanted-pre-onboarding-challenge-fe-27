bec9b9e38cc198b467cd111e29c69c6c7be9f90f
jiwlee <53519832+jiwlee97@users.noreply.github.com>
2024-10-31 17:52:25 +0900

    chore: temp

--- src/pages/todos/ui/Todos.tsx
+++ src/pages/todos/ui/Todos.tsx
@@ -1,9 +1,77 @@
-import { useTodos } from "@/entities/todo";
-import { Button } from "@/shared/ui";
+import { Todo, useTodos } from "@/entities/todo";
+import { useDeleteTodo } from "@/features/deleteTodo";
+import { useToast } from "@/shared/lib";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+  Button,
+  Icon,
+} from "@/shared/ui";
 import { Link, useLocation, useNavigate } from "react-router-dom";
+
+const DeleteTodoButton = ({ todoId }: { todoId: Todo["id"] }) => {
+  const navigate = useNavigate();
+  const { toast } = useToast();
+
+  const { mutateAsync } = useDeleteTodo();
+
+  const deleteTodo = async () => {
+    await mutateAsync(todoId, {
+      onError: () => {
+        console.log("error");
+        toast({
+          variant: "destructive",
+          title: "할 일 삭제에 실패했습니다. 다시 시도해주세요.",
+        });
+      },
+      onSuccess: () => {
+        console.log("success");
+        toast({
+          variant: "success",
+          title: "할 일이 삭제되었습니다.",
+        });
+        navigate("/todos");
+      },
+    });
+  };
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button
+          variant="ghost"
+          className="opacity-0 w-3 h-3 p-2 border-none bg-transparent hover:border-none hover:bg-slate-200 group-hover:opacity-100"
+        >
+          <Icon name="close" size="small" />
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={deleteTodo}>Delete</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
 
 export const Todos = () => {
   const { data } = useTodos();
+
   const location = useLocation();
   const navigate = useNavigate();
 
@@ -16,17 +84,21 @@ export const Todos = () => {
       <nav className="flex space-x-2 lg:flex-col lg:space-x-0 lg:space-y-1">
         <Button onClick={navigateToAddTodo}>Add Todo</Button>
         {data.map((todo) => (
-          <Link
-            key={todo.id}
-            to={`/todos/${todo.id}`}
-            className={`inline-flex items-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 hover:text-accent-foreground h-9 px-4 py-2 justify-start ${
-              location.pathname === `/todos/${todo.id}`
-                ? "bg-muted hover:bg-muted"
-                : "hover:bg-transparent hover:underline"
-            }`}
-          >
-            {todo.title}
-          </Link>
+          <div key={todo.id} className="w-full">
+            <Link
+              to={`/todos/${todo.id}`}
+              className={`w-full group inline-flex items-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 hover:text-accent-foreground h-9 px-4 py-2 justify-start ${
+                location.pathname === `/todos/${todo.id}`
+                  ? "bg-muted hover:bg-muted"
+                  : "hover:bg-transparent hover:underline"
+              }`}
+            >
+              <div className="w-full flex justify-between items-center gap-1">
+                {todo.title}
+                <DeleteTodoButton todoId={todo.id} />
+              </div>
+            </Link>
+          </div>
         ))}
       </nav>
     </aside>
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -25,3 +25,16 @@ export { DefaultPageLayout } from './layouts/DefaultPageLayout';
 export { CheckAuth } from './layouts/CheckAuth';
 export { Textarea } from './Textarea';
 export { Icon } from './Icon';
+export {
+  AlertDialog,
+  AlertDialogPortal,
+  AlertDialogOverlay,
+  AlertDialogTrigger,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogFooter,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogAction,
+  AlertDialogCancel,
+} from './AlertDialog';
